3e7a5e5364ec977ed702c9abf1575eab14160465
melekozkan91 <ozkanmelek1293@gmail.com>
2023-04-05 17:10:50 -0700

    day_24_office_hours_1

--- src/my_util/StringUtil.java
+++ src/my_util/StringUtil.java
@@ -13,11 +13,32 @@ public class StringUtil {
     public static String reverseStr (String str){
         String reserved ="";
         for (int i = str.length()-1; i >=0 ; i--) {
-            reserved +=str.charAt(i);
+            reserved += str.charAt(i);
 
 
         }
         return reserved;
+        /**
+         * This method made by Loopcamp Batch#1
+         * This method accepts a String parameter and puts it in a format
+         * Ex:
+         *    The format will be like this
+         *    First letter is in upper case and rest is in lower case
+         *
+         * Then return a String in a formatted way
+
+         */
+
+
+
+
+
+    }
+    public static String fixFormat(String str){ // __feyruz__ --F
+        String fixed =str.trim();
+         fixed = str.substring(0,1).toUpperCase()+str.substring(1).toLowerCase();
+        return fixed.trim();
+
 
     }
 
